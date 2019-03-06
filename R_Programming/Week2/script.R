07747491d3eb540c28330078777202f6ff04ad29
horaciochacon <horacio.chacon.t@upch.pe>
2019-03-06 11:26:18 -0500

    Archivos para tarea 2

---
+++ R_Programming/Week2/script.R
@@ -0,0 +1,10 @@
+
+pollutantmean <- function(directory, pollutant, id){
+  
+  for (i in 1:id) {
+    read.csv(paste(directory,formatC(id,width = 3, flag = "0"),".csv",sep = ""))
+  } 
+  
+  
+  
+}
