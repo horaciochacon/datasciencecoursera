779120530187ac4b5eaf1864edf05f1003304d94
horaciochacon <horacio.chacon.t@upch.pe>
2019-03-18 00:19:51 -0500

    Week4-best

--- R_Programming/Week4/Programming_assignment3.R
+++ R_Programming/Week4/Programming_assignment3.R
@@ -3,8 +3,26 @@
 outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character") 
 
 best <- function(state, outcome) {
-    data <- read.csv("outcome-of-care-measures.csv")
-    data <- data[,c(2,7,grep("Lower",names(data)))]
+    # Reads data and returns only variables of interest via a "Type" grep input
+    readData <- function(type) {
+        data <- read.csv("outcome-of-care-measures.csv")
+        data <- data[,c(2,7,grep(type,names(data)))]
+        names <- sapply(strsplit(names(data),".from.")[3:5],"[[",2)
+        names(data)[3:5] <- names
+        data
+    }
     
     
+    checker <- function(state, outcome) {
+        states <- unique(data[,2])
+        outcomes <- c("heart attack","pneumonia", "heart failure")
+        check <- state %in% states
+        check <- c(check,outcome %in% outcomes)
+        check
+    }
+    
+    sor
+    
+    data <- readData(type = "Lower.Mort")
+    checker(state, outcome)
 }
